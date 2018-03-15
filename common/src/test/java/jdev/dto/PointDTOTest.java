25128f4dbeed96c6683762f0afb91e57ec361d41
Vyacheslav <slava.borgir@gmail.com>
2018-03-15 23:09:06 +0700

    Lab2

--- common/src/test/java/jdev/dto/PointDTOTest.java
+++ common/src/test/java/jdev/dto/PointDTOTest.java
@@ -8,14 +8,14 @@ import static org.junit.Assert.assertTrue;
 
 public class PointDTOTest {
 
-    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1520422523797}";
+    private String json = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1520422523797}";
     private String autoId = "o567gfd";
 
     @Test
     /**
      * Из объетка в строку
      */
-    public void toJson() throws Exception {
+    public void encodeDto() throws Exception {
         PointDTO point = new PointDTO();
         point.setLat(56);
         point.setLon(74);
@@ -32,7 +32,7 @@ public class PointDTOTest {
      */
     public void decodeDTO() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        PointDTO dto = mapper.readValue(expected, PointDTO.class);
+        PointDTO dto = mapper.readValue(json, PointDTO.class);
         assertEquals(autoId, dto.getAutoId());
         assertEquals(1520422523797L, dto.getTime());
     }
