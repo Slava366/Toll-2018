637b671950ec99c9e642d21e6691ceb6e96d0ebd
Vyacheslav <slava.borgir@gmail.com>
2018-03-25 14:57:15 +0700

    Возвращение тестов в модули

--- tracker-core/src/test/java/jdev/dto/StateTest.java
+++ tracker-core/src/test/java/jdev/dto/StateTest.java
@@ -8,32 +8,32 @@ import static org.junit.Assert.assertTrue;
 
 public class StateTest {
 
-    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1520422523797}";
+    private String json = "{\"latitude\":56.0,\"longitude\":74.0,\"autoId\":\"o567gfd\",\"time\":1521964156074,\"speed\":0.0,\"azimuth\":0.0}";
     private String autoId = "o567gfd";
 
-    @Test
     /**
      * Из объетка в строку
      */
-    public void toJson() throws Exception {
+    @Test
+    public void encodeDto() throws Exception {
         State state = new State();
         state.setLatitude(56);
         state.setLongitude(74);
         state.setAutoId("o567gfd");
         state.setTime(System.currentTimeMillis());
-        assertTrue(state.toJson().contains("\"lat\":56"));
+        assertTrue(state.toJson().contains("\"latitude\":56"));
         assertTrue(state.toJson().contains("\"time\":"));
         System.out.println(state.toJson());
     }
 
-    @Test
     /**
      * Из строки в объект
      */
+    @Test
     public void decodeDTO() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        State dto = mapper.readValue(expected, State.class);
+        State dto = mapper.readValue(json, State.class);
         assertEquals(autoId, dto.getAutoId());
-        assertEquals(1520422523797L, dto.getTime());
+        assertEquals(1521964156074L, dto.getTime());
     }
 }
