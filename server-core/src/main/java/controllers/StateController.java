cf0efb0136912e01145c34a8380d42a942bceb01
Vyacheslav <slava.borgir@gmail.com>
2018-03-31 17:46:19 +0700

    Редактирование StateController для приема POST запросов

--- server-core/src/main/java/controllers/StateController.java
+++ server-core/src/main/java/controllers/StateController.java
@@ -1,12 +1,26 @@
 package controllers;
 
+import jdev.dto.State;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class StateController {
 
+    private static Logger LOG = LoggerFactory.getLogger(StateController.class);
+
     @Autowired
     RestTemplate restTemplate;
+
+    @RequestMapping(value = "/state", method = RequestMethod.POST)
+    public void getState(@RequestParam(value = "stateInfo") String stateInfo) {
+        // Выводим в лог информацию о состоянии
+        LOG.info(stateInfo);
+    }
 }
