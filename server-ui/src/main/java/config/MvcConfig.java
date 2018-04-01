729d27e1c858e0dc88d7a7064ce3f454eb913872
Vyacheslav <slava.borgir@gmail.com>
2018-04-01 18:05:25 +0700

    Создаем приложение и формируем MvcConfig

---
+++ server-ui/src/main/java/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/routes").setViewName("routes");
+        registry.addViewController("/payments").setViewName("payments");
+        registry.addViewController("/registerClient").setViewName("registerClient");
+        registry.addViewController("/registerManager").setViewName("registerManager");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/error").setViewName("error");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+    }
+}
