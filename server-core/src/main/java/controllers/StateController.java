bb04aa986df981094ac0d58deab080fd35e06c1d
Vyacheslav <slava.borgir@gmail.com>
2018-04-01 16:25:39 +0700

    Lab5

--- server-core/src/main/java/controllers/StateController.java
+++ server-core/src/main/java/controllers/StateController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
 
 @RestController
 public class StateController {
