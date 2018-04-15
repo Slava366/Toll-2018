a0b969f8aba07721303a58705d105b8a2a1de161
Vyacheslav <slava.borgir@gmail.com>
2018-04-15 20:39:37 +0700

    Добавление unit тестов в common

--- common/src/test/java/jdev/dto/StateTest.java
+++ common/src/test/java/jdev/dto/StateTest.java
@@ -1,10 +1,12 @@
 package jdev.dto;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.*;
 
 public class StateTest {
 
@@ -15,7 +17,7 @@ public class StateTest {
      * Из объетка в строку
      */
     @Test
-    public void encodeDto() throws Exception {
+    public void encodeDTO() throws Exception {
         State state = new State();
         state.setLatitude(56);
         state.setLongitude(74);
@@ -35,5 +37,105 @@ public class StateTest {
         State dto = mapper.readValue(json, State.class);
         assertEquals(autoId, dto.getAutoId());
         assertEquals(1521964156074L, dto.getTime());
+    }
+
+    @Test
+    public void getLatitude() {
+        State state = new State();
+        state.setLatitude(89);
+        assertEquals(89, state.getLatitude(), 0);
+    }
+
+    @Test
+    public void setLatitude() {
+        State state = new State();
+        state.setLatitude(89);
+        assertEquals(89, state.getLatitude(), 0);
+    }
+
+    @Test
+    public void getLongitude() {
+        State state = new State();
+        state.setLongitude(89);
+        assertEquals(89, state.getLongitude(), 0);
+    }
+
+    @Test
+    public void setLongitude() {
+        State state = new State();
+        state.setLongitude(89);
+        assertEquals(89, state.getLongitude(), 0);
+    }
+
+    @Test
+    public void getAutoId() {
+        State state = new State();
+        state.setAutoId("a666dy");
+        assertEquals("a666dy", state.getAutoId());
+        assertNotNull(state.getAutoId());
+    }
+
+    @Test
+    public void setAutoId() {
+        State state = new State();
+        state.setAutoId("a666dy");
+        assertEquals("a666dy", state.getAutoId());
+        assertNotNull(state.getAutoId());
+    }
+
+    @Test
+    public void setTime() {
+        State state = new State();
+        state.setTime(3265487946L);
+        assertEquals(3265487946L, state.getTime());
+        assertNotEquals(6548972L, state.getTime());
+    }
+
+    @Test
+    public void getTime() {
+        State state = new State();
+        state.setTime(3265487946L);
+        assertEquals(3265487946L, state.getTime());
+        assertNotEquals(6548972L, state.getTime());
+    }
+
+    @Test
+    public void getSpeed() {
+        State state = new State();
+        state.setSpeed(200);
+        assertEquals(200, state.getSpeed(), 0);
+        assertNotEquals(56, state.getSpeed());
+    }
+
+    @Test
+    public void setSpeed() {
+        State state = new State();
+        state.setSpeed(200);
+        assertEquals(200, state.getSpeed(), 0);
+        assertNotEquals(56, state.getSpeed());
+    }
+
+    @Test
+    public void getAzimuth() {
+        State state = new State();
+        state.setAzimuth(98);
+        assertEquals(98, state.getAzimuth(), 0);
+        assertNotEquals(31, state.getAzimuth());
+    }
+
+    @Test
+    public void setAzimuth() {
+        State state = new State();
+        state.setAzimuth(98);
+        assertEquals(98, state.getAzimuth(), 0);
+        assertNotEquals(31, state.getAzimuth());
+    }
+
+    @Test
+    public void toJson() throws JsonProcessingException {
+        State state = new State();
+        for(Field field : state.getClass().getDeclaredFields()) {
+            assertTrue(state.toJson().contains(field.getName()));
+        }
     }
 }
