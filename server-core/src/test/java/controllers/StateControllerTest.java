4dae59613b8060ea707caadcf771c62d6b51bda2
Vyacheslav <slava.borgir@gmail.com>
2018-04-29 22:02:41 +0700

    Редактируем StateControllerTest

--- server-core/src/test/java/controllers/StateControllerTest.java
+++ server-core/src/test/java/controllers/StateControllerTest.java
@@ -1,24 +1,33 @@
 package controllers;
 
+import jdev.dto.repo.StateRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class StateControllerTest {
 
-    private String json = "{\"latitude\":56.0,\"longitude\":74.0,\"autoId\":\"o567gfd\",\"time\":1521964156074,\"speed\":95.3,\"azimuth\":89.1}";
+    private String json = "{\"id\":0,\"latitude\":56.0,\"longitude\":74.0,\"autoId\":\"o567gfd\",\"time\":1521964156074,\"speed\":95.3,\"azimuth\":89.1}";
+
+    @Mock
+    StateRepository stateRepository;
+
+    @InjectMocks
+    StateController mockedController;
 
     @Test
     public void getState() {
-        // Создаем объект
-        StateController controller = new StateController();
         // Отправляем неверную строку
-        ServerResponse response = controller.getState("state");
-        // Проверяем правильность результата
+        ServerResponse response = mockedController.getState("State");
         assertEquals(false, response.isSuccess());
         // Отправляем корректную строку json состояния
-        response = controller.getState(json);
-        // Проверяем правильность результата
+        response = mockedController.getState(json);
         assertEquals(true, response.isSuccess());
     }
 }
