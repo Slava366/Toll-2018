d27ec80b3008885c2813788c361dd15c850e8245
Vyacheslav <slava.borgir@gmail.com>
2018-05-05 11:18:40 +0700

    Редактируем build.gradle

--- common/src/main/java/jdev/dto/repo/StateRepository.java
+++ common/src/main/java/jdev/dto/repo/StateRepository.java
@@ -2,6 +2,8 @@ package jdev.dto.repo;
 
 import jdev.dto.State;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StateRepository extends CrudRepository<State, Integer> {
 }
