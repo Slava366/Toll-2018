209516656cb6e064f2955033f973cbe9f2acab5c
Vyacheslav <slava.borgir@gmail.com>
2018-05-06 13:17:24 +0700

    Начинаем выполнять задание повышенной сложности, подключаем БД к server-ui

--- server-ui/src/main/java/security/Application.java
+++ server-ui/src/main/java/security/Application.java
@@ -2,12 +2,23 @@ package security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("config")
+@EnableJpaRepositories({"jdev.dto"})
+@EntityScan(basePackageClasses = {jdev.dto.State.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
