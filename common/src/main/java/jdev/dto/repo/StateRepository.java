b8680a274a8306d75e83de21ed6967ef5b782b07
Vyacheslav <slava.borgir@gmail.com>
2018-05-06 12:49:54 +0700

    Создаем запрос в репозитарии для выборки по autoId

--- common/src/main/java/jdev/dto/repo/StateRepository.java
+++ common/src/main/java/jdev/dto/repo/StateRepository.java
@@ -4,6 +4,9 @@ import jdev.dto.State;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface StateRepository extends CrudRepository<State, Integer> {
+    public ArrayList<State> findStatesByAutoIdIs(String autoId);
 }
