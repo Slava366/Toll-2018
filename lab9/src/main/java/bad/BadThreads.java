9754d0631a42de4a6df69f1593170578e32182d6
Vyacheslav <slava.borgir@gmail.com>
2018-05-12 17:34:50 +0700

    Добавление класса BadThreads и анализ его работы, 4-й вариант

--- lab9/src/main/java/bad/BadThreads.java
+++ lab9/src/main/java/bad/BadThreads.java
@@ -2,29 +2,31 @@ package bad;
 
 /**
  * Должен выводить в консоль "Помиловать" 1000 раз, однако это происходит не всегда.
- * 3-й вариант, ждем завершения нити корректора:
- * Результат: "Казнить" не появляется.
+ * 4-й вариант, используем synchronized:
+ * Результат: Проблема не решена.
  */
 public class BadThreads {
 
     static String message;
-    static Object lock = new Object();
+    final static Object lock = new Object();
 
     private static class CorrectorThread extends Thread {
         public void run() {
-            message = "Помиловать";
+            synchronized (lock) {
+                message = "Помиловать";
+            }
         }
     }
 
     public static void main(String args[]) throws InterruptedException {
         for (int i=0; i<1000; i++) {
             CorrectorThread correctorThread = new CorrectorThread();
-            message = "Казнить";
+            synchronized (lock) {
+                message = "Казнить";
+            }
             correctorThread.start();
-//            Thread.sleep(10);
-            correctorThread.join();
-            if (message.equalsIgnoreCase("Казнить"))
-                System.out.println(message);
+            Thread.sleep(10);
+            if (message.equalsIgnoreCase("Казнить")) System.out.println(message);
         }
     }
 }
