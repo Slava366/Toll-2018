6ab3537cd5ab61fd41ef71d0b9307f965ff633db
Vyacheslav <slava.borgir@gmail.com>
2018-05-12 17:50:00 +0700

    Добавление класса BadThreads и анализ его работы, 6-й вариант

--- lab9/src/main/java/bad/BadThreads.java
+++ lab9/src/main/java/bad/BadThreads.java
@@ -5,43 +5,34 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Должен выводить в консоль "Помиловать" 1000 раз, однако это происходит не всегда.
- * 5-й вариант, используем lock:
+ * 6-й вариант, используем synchronized:
  * Результат: Проблема не решена.
  */
 public class BadThreads {
 
-    static String message;
-    final static Object lock = new Object();
-    public final static ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private static String message;
+
+    private static synchronized void setMessage(String message) {
+        BadThreads.message = message;
+    }
+
+    private static synchronized String getMessage(){
+        return BadThreads.message;
+    }
 
     private static class CorrectorThread extends Thread {
         public void run() {
-            BadThreads.readWriteLock.writeLock().lock();
-            try {
-                message = "Помиловать";
-            } finally {
-                BadThreads.readWriteLock.writeLock().unlock();
-            }
+            setMessage("Помиловать");
         }
     }
 
     public static void main(String args[]) throws InterruptedException {
         for (int i=0; i<1000; i++) {
             CorrectorThread correctorThread = new CorrectorThread();
-            BadThreads.readWriteLock.writeLock().lock();
-            try {
-                message = "Казнить";
-            } finally {
-                BadThreads.readWriteLock.writeLock().unlock();
-            }
+            setMessage("Казнить");
             correctorThread.start();
             Thread.sleep(10);
-            BadThreads.readWriteLock.readLock().lock();
-            try {
-                if (message.equalsIgnoreCase("Казнить")) System.out.println(message);
-            } finally {
-                BadThreads.readWriteLock.readLock().unlock();
-            }
+            if (getMessage().equalsIgnoreCase("Казнить")) System.out.println(getMessage());
         }
     }
 }
