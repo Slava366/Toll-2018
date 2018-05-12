bc1b6061d7edda00030ede1c662b7fb00af84bcc
Vyacheslav <slava.borgir@gmail.com>
2018-05-12 17:45:40 +0700

    Добавление класса BadThreads и анализ его работы, 5-й вариант

--- lab9/src/main/java/bad/BadThreads.java
+++ lab9/src/main/java/bad/BadThreads.java
@@ -1,19 +1,26 @@
 package bad;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * Должен выводить в консоль "Помиловать" 1000 раз, однако это происходит не всегда.
- * 4-й вариант, используем synchronized:
+ * 5-й вариант, используем lock:
  * Результат: Проблема не решена.
  */
 public class BadThreads {
 
     static String message;
     final static Object lock = new Object();
+    public final static ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     private static class CorrectorThread extends Thread {
         public void run() {
-            synchronized (lock) {
+            BadThreads.readWriteLock.writeLock().lock();
+            try {
                 message = "Помиловать";
+            } finally {
+                BadThreads.readWriteLock.writeLock().unlock();
             }
         }
     }
@@ -21,12 +28,20 @@ public class BadThreads {
     public static void main(String args[]) throws InterruptedException {
         for (int i=0; i<1000; i++) {
             CorrectorThread correctorThread = new CorrectorThread();
-            synchronized (lock) {
+            BadThreads.readWriteLock.writeLock().lock();
+            try {
                 message = "Казнить";
+            } finally {
+                BadThreads.readWriteLock.writeLock().unlock();
             }
             correctorThread.start();
             Thread.sleep(10);
-            if (message.equalsIgnoreCase("Казнить")) System.out.println(message);
+            BadThreads.readWriteLock.readLock().lock();
+            try {
+                if (message.equalsIgnoreCase("Казнить")) System.out.println(message);
+            } finally {
+                BadThreads.readWriteLock.readLock().unlock();
+            }
         }
     }
 }
