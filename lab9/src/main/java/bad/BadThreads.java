512ab77b7e86962bff0f688e2e8c633211e9ffde
Vyacheslav <slava.borgir@gmail.com>
2018-05-12 17:52:56 +0700

    Добавление класса BadThreads и анализ его работы, 7-й вариант, проблема решена

--- lab9/src/main/java/bad/BadThreads.java
+++ lab9/src/main/java/bad/BadThreads.java
@@ -5,34 +5,26 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Должен выводить в консоль "Помиловать" 1000 раз, однако это происходит не всегда.
- * 6-й вариант, используем synchronized:
- * Результат: Проблема не решена.
+ * 7-й вариант, дожидаемся завершения нити корректора:
+ * Результат: Проблема решена на 100%.
  */
 public class BadThreads {
 
     private static String message;
 
-    private static synchronized void setMessage(String message) {
-        BadThreads.message = message;
-    }
-
-    private static synchronized String getMessage(){
-        return BadThreads.message;
-    }
-
     private static class CorrectorThread extends Thread {
         public void run() {
-            setMessage("Помиловать");
+            message = "Помиловать";
         }
     }
 
     public static void main(String args[]) throws InterruptedException {
         for (int i=0; i<1000; i++) {
             CorrectorThread correctorThread = new CorrectorThread();
-            setMessage("Казнить");
+            message = "Казнить";
             correctorThread.start();
-            Thread.sleep(10);
-            if (getMessage().equalsIgnoreCase("Казнить")) System.out.println(getMessage());
+            correctorThread.join();
+            if (message.equalsIgnoreCase("Казнить")) System.out.println(message);
         }
     }
 }
